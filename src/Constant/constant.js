ec9b0cc9b67683cd4b3ff7dbd833f5982d92e7c7
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-06-14 19:25:21 +0530

    Update - Added DB routes

--- src/Constant/constant.js
+++ src/Constant/constant.js
@@ -2,3 +2,6 @@ export const SUCCESS = "SUCCESS";
 export const INFO = "INFO";
 export const WARNING = "WARNING";
 export const DANGER = "DANGER";
+export const DEFAULT_AVATAR = "/Images/avatarDefault.png";
+export const LOCALTALKS_POSTS = "localTalks-posts";
+export const LOCALTALKS_USERS = "localTalks-users";
