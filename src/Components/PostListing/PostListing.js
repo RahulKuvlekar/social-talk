7db018169d4075b6cea7d67215ccb2a5b87f18aa
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-06-20 06:42:17 +0530

    Refactor  - removed and clean code

--- src/Components/PostListing/PostListing.js
+++ src/Components/PostListing/PostListing.js
@@ -1,28 +1,13 @@
-import { useEffect } from "react";
+import React from "react";
 import "./PostListing.css";
-import { useDispatch, useSelector } from "react-redux";
-import { getAuthData } from "../../Pages/Authentication/AuthenticationSlice";
-import { getAllPost, getPostData } from "../PostSlice/PostSlice";
-import { ADD_TOAST } from "../UI/Toast/ToastSlice";
-import { DANGER } from "../../Constant/constant";
+import { useSelector } from "react-redux";
 import Post from "../Post/Post";
 import Loader from "../UI/Loader/Loader";
+import { getPostData } from "../PostSlice/PostSlice";
 
 const PostListing = () => {
-  const dispatch = useDispatch();
-  const { isAuthenticated } = useSelector(getAuthData);
   const { allPosts, postLoading } = useSelector(getPostData);
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      dispatch(getAllPost())
-        .unwrap()
-        .then()
-        .catch((error) => {
-          dispatch(ADD_TOAST(DANGER, error.message));
-        });
-    }
-  }, [isAuthenticated, dispatch]);
   return (
     <div>
       {postLoading && <Loader />}
