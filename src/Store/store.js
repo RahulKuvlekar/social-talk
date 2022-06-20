59f24ca7ed54932f11673089d6f6c3a59c9e249b
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-06-20 06:33:38 +0530

    Update - Added MyProfile slice to store, Middleware setting

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,12 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import authenticationReducer from "../Pages/Authentication/AuthenticationSlice";
 import toastReducer from "../Components/UI/Toast/ToastSlice";
 import postReducer from "../Components/PostSlice/PostSlice";
+import myProfileReducer from "../Components/MyProfile/MyProfileSlice";
 
 const store = configureStore({
   reducer: {
     authentication: authenticationReducer,
     toast: toastReducer,
     post: postReducer,
+    myProfile: myProfileReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["Authentication/SET_USER"],
+        ignoredPaths: ["authentication.user", "payload"],
+      },
+    }),
 });
 export { store };
